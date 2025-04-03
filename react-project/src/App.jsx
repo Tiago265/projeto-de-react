8c2334e3a45add02ec0fbc6a0432ef660ca97cce
Tiago265 <futlastf998@gmail.com>
2025-04-03 09:18:33 -0300

    projeto de react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,10 @@
+import ('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Play:wght@400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+
 import './App.css'
 
 import { IoLogoGithub } from "react-icons/io";
+
 import Instagram from './components/Instagram';
 
 function App() {
--- react-project/src/components/Instagram.jsx
+++ react-project/src/components/Instagram.jsx
@@ -1,3 +1,7 @@
+
+import('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Play:wght@400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+
 import React from 'react'
 
 import { FaInstagram } from "react-icons/fa";
@@ -11,7 +15,7 @@ function Instagram() {
 
     <div className='container-instagram '>
 
-        <a href="" className='links' target='_blank'>
+        <a href="https://www.instagram.com/tiago_burlamaque2/" className='links-dois' target='_blank'>
             
         <FaInstagram />
 
