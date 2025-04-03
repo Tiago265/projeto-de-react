14d5b1c8fca8ead898d9957f108756885de0fa95
Tiago265 <futlastf998@gmail.com>
2025-04-02 22:14:49 -0300

    projeto

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css'
+
 import { IoLogoGithub } from "react-icons/io";
+import Instagram from './components/Instagram';
 
 function App() {
  
   return (
-    <div className='container-app'>
+
+    <div className ='container-app'>
+
       <h1>Links do Tiago</h1>
 
       <a href="https://github.com/Tiago265" className='links' target='_blank'>
+
       <IoLogoGithub />
+
       Github
+
       </a>
       
+      <Instagram />
+
     </div>
+
+
   )
+
 }
 
 export default App
---
+++ react-project/src/components/Instagram.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { FaInstagram } from "react-icons/fa";
+
+import './instagram.css'
+
+
+function Instagram() {
+
+  return (
+
+    <div className='container-instagram '>
+
+        <a href="" className='links' target='_blank'>
+            
+        <FaInstagram />
+
+             Instagram
+            
+            </a>
+
+    </div>
+
+  )
+
+}
+
+export default Instagram
