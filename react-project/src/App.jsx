faec18fb284162f6c5fa38599eaa390c80527be1
Tiago265 <futlastf998@gmail.com>
2025-04-02 21:14:19 -0300

    projeto

---
+++ react-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { IoLogoGithub } from "react-icons/io";
+
+function App() {
+ 
+  return (
+    <div className='container-app'>
+      <h1>Links do Tiago</h1>
+
+      <a href="https://github.com/Tiago265" className='links' target='_blank'>
+      <IoLogoGithub />
+      Github
+      </a>
+      
+    </div>
+  )
+}
+
+export default App
